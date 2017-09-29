8a221c3069544ac8172b918c7b97584deec642e2
guldfisk <ce.guldfisk@gmail.com>
2017-09-29 16:02:52 +0200

    wauw

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setup(
 	packages=['mtgorp'],
 	package_data={'': extra_files},
 	dependency_links=[
-		'https://github.com/guldfisk/orp/tarball/master#egg=package-1.0',
-		# 'git+ssh://git@github.com/guldfisk/orp.git@1.0#egg=package-1.0',
+		'git+https://github.com/guldfisk/orp.git#egg=orp-1.0',
+		# 'git+ssh://git@github.com/guldfisk/orp.git@1.0#egg=orp-1.0',
 	],
 	install_requires=[
 		'orp==1.0',
