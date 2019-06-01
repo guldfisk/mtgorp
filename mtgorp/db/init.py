f8d93f7460ab7c270f2d663c7c42245dee8f9875
guldfisk <ce.guldfisk@gmail.com>
2019-06-01 16:47:45 +0200

    :)

--- mtgorp/db/init.py
+++ mtgorp/db/init.py
@@ -1,15 +1,15 @@
-
-import time
-
 from mtgorp.managejson.update import check_and_update
 from mtgorp.db.create import update_database
-from mtgorp.db.load import Loader
 
 
-
-def test():
-   print('huh')
+def mtgdb_init():
+    if check_and_update():
+        print('New magic json')
+        update_database()
+        print('Database updated')
+    else:
+        print('Magic db up to date')
 
 
 if __name__ == '__main__':
-    test()
+    mtgdb_init()
