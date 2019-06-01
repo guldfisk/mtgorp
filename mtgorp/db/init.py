b1d4439e2a2912b60febb2729f26dc81ec2abe5e
guldfisk <ce.guldfisk@gmail.com>
2019-06-01 16:39:50 +0200

    :)

---
+++ mtgorp/db/init.py
@@ -0,0 +1,15 @@
+
+import time
+
+from mtgorp.managejson.update import check_and_update
+from mtgorp.db.create import update_database
+from mtgorp.db.load import Loader
+
+
+
+def test():
+   print('huh')
+
+
+if __name__ == '__main__':
+    test()
