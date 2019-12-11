ef742874fdd4cb40fddc8c43a35adda458d31c7a
carl-emil hjort-trærup <ce.guldfisk@gmail.com>
2019-12-11 15:37:36 +0100

    ok

--- setup.py
+++ setup.py
@@ -3,31 +3,31 @@ import os
 
 
 def package_files(directory):
-	paths = []
-	for path, directories, file_names in os.walk(directory):
-		for filename in file_names:
-			paths.append(os.path.join('..', path, filename))
-	return paths
+    paths = []
+    for path, directories, file_names in os.walk(directory):
+        for filename in file_names:
+            paths.append(os.path.join('..', path, filename))
+    return paths
 
 extra_files = package_files('mtgorp')
 
 setup(
-	name='mtgorp',
-	version='1.0',
-	packages=['mtgorp'],
-	package_data={'': extra_files},
-	dependency_links=[
-		'https://github.com/guldfisk/orp/tarball/master#egg=orp-1.0',
-		'https://github.com/guldfisk/yeetlong/tarball/master#egg=yeetlong-1.0',
-	],
-	install_requires=[
-		'orp',
-		'yeetlong',
-		'lazy-property',
-		'multiset',
-		'appdirs',
-		'frozendict',
-		'requests',
-		'antlr4-python3-runtime',
-	]
+    name='mtgorp',
+    version='1.0',
+    packages=['mtgorp'],
+    package_data={'': extra_files},
+    # dependency_links=[
+    # 	'https://github.com/guldfisk/orp/tarball/master#egg=orp-1.0',
+    # 	'https://github.com/guldfisk/yeetlong/tarball/master#egg=yeetlong-1.0',
+    # ],
+    install_requires=[
+        'orp @ https://github.com/guldfisk/orp/tarball/master#egg=orp-1.0',
+        'yeetlong @ https://github.com/guldfisk/yeetlong/tarball/master#egg=yeetlong-1.0',
+        'lazy-property',
+        'multiset',
+        'appdirs',
+        'frozendict',
+        'requests',
+        'antlr4-python3-runtime',
+    ]
 )
