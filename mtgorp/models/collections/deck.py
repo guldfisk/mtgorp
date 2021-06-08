cc3ec405c4f98c828e059c463fd05ef5dcdd7a33
carl-emil hjort-trærup <ce.guldfisk@gmail.com>
2021-06-08 15:26:44 +0200

    ok

--- mtgorp/models/collections/deck.py
+++ mtgorp/models/collections/deck.py
@@ -1,27 +1,33 @@
 from __future__ import annotations
 
 import typing as t
+from abc import abstractmethod
 
 from yeetlong.multiset import FrozenMultiset
 
-from mtgorp.models.interfaces import Printing
+from mtgorp.models.interfaces import Printing, Cardboard
 from mtgorp.models.serilization.serializeable import Serializeable, serialization_model, Inflator
 
 
-class Deck(Serializeable):
+Deckable = t.Union[Printing, Cardboard]
+D = t.TypeVar('D', bound = Deckable)
+
+
+class BaseDeck(t.Generic[D], Serializeable):
+    _maindeck: FrozenMultiset[D]
 
     def __init__(
         self,
-        maindeck: t.Union[t.Iterable[Printing], t.Iterable[t.Tuple[Printing, int]]],
-        sideboard: t.Union[t.Iterable[Printing], t.Iterable[t.Tuple[Printing, int]], None] = None,
+        maindeck: t.Union[t.Iterable[D], t.Iterable[t.Tuple[D, int]]],
+        sideboard: t.Union[t.Iterable[D], t.Iterable[t.Tuple[D, int]], None] = None,
     ):
-        self._maindeck: FrozenMultiset[Printing] = (
+        self._maindeck: FrozenMultiset[D] = (
             maindeck
             if isinstance(maindeck, FrozenMultiset)
             else FrozenMultiset(maindeck)
         )
 
-        self._sideboard: FrozenMultiset[Printing] = (
+        self._sideboard: FrozenMultiset[D] = (
             (
                 sideboard
                 if isinstance(sideboard, FrozenMultiset) else
@@ -32,23 +38,23 @@ class Deck(Serializeable):
         )
 
     @property
-    def maindeck(self) -> FrozenMultiset[Printing]:
+    def maindeck(self) -> FrozenMultiset[D]:
         return self._maindeck
 
     @property
-    def sideboard(self) -> FrozenMultiset[Printing]:
+    def sideboard(self) -> FrozenMultiset[D]:
         return self._sideboard
 
     @property
-    def seventy_five(self) -> FrozenMultiset[Printing]:
+    def seventy_five(self) -> FrozenMultiset[D]:
         return self._maindeck + self._sideboard
 
-    def __iter__(self) -> t.Iterable[Printing]:
+    def __iter__(self) -> t.Iterable[D]:
         return self.seventy_five.__iter__()
 
     def __eq__(self, other) -> bool:
         return (
-            isinstance(other, Deck)
+            isinstance(other, self.__class__)
             and self._maindeck == other.maindeck
             and self._sideboard == other.sideboard
         )
@@ -66,8 +72,33 @@ class Deck(Serializeable):
         }
 
     @classmethod
+    @abstractmethod
+    def deserialize(cls, value: serialization_model, inflator: Inflator) -> BaseDeck:
+        pass
+
+
+class CardboardDeck(BaseDeck[Cardboard]):
+
+    @classmethod
+    def deserialize(cls, value: serialization_model, inflator: Inflator) -> BaseDeck:
+        return cls(
+            inflator.inflate_all(Cardboard, value['maindeck']),
+            inflator.inflate_all(Cardboard, value.get('sideboard', ())),
+        )
+
+
+class Deck(BaseDeck[Printing]):
+
+    @classmethod
     def deserialize(cls, value: serialization_model, inflator: Inflator) -> Deck:
-        return Deck(
+        return cls(
             inflator.inflate_all(Printing, value['maindeck']),
             inflator.inflate_all(Printing, value.get('sideboard', ())),
+        )
+
+    @property
+    def as_cardboards(self):
+        return CardboardDeck(
+            FrozenMultiset(p.cardboard for p in self._maindeck),
+            FrozenMultiset(p.cardboard for p in self._sideboard),
         )
